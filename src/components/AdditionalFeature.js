58682a20a11ea124615d6eb4a220d7d9b1daa76d
lljmb <brown.jessm@icloud.com>
2021-01-19 20:46:10 -0500

    completed car sales app

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { addFeatures } from '../actions/carActions';
+import { connect } from 'react-redux';
 
 const AdditionalFeature = props => {
+
+  const handleClick = () => {
+    props.addFeatures(props.feature)
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={handleClick} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+// use connect to connect state to rest of App 
+export default connect(null, { addFeatures })(AdditionalFeature)
